5c10ee0031cd75be7d8f333214a23f30f07f7da5
Storchillarn <gurr_gurr@hotmail.com>
2019-06-16 19:31:20 +0200

    Animate menu. Add throttling to scroll-event

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@ import './img/background.jpg';
 import './main.sass';
 
 window.onload = () => {
-    const background = document.querySelector('HEADER');
-    window.onscroll = () => {
-        if (window.scrollY > 0) {
-            background.classList.add('--fade-in');
+
+    let scrolling = false;
+
+    const nav = document.querySelector('NAV');
+
+    window.addEventListener('scroll', () => scrolling = true);
+
+    setInterval(() => {
+        if (scrolling) {
+            scrolling = !scrolling;
+            scrollHandler(nav);
+        }
+    }, 250);
+
+    function scrollHandler(elem) {
+        if (window.scrollY >= 100) {
+            elem.classList.add('main-nav-scroll');
+        }
+        else if (window.scrollY < 100) {
+            elem.classList.remove('main-nav-scroll');
         }
     }
 }
